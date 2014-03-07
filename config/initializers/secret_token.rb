102a87d89e7914151510904cad56455862785540
theInternauts <cricketw@wearetheinternauts.com>
2014-03-06 17:23:50 -0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MagazineMaker::Application.config.secret_key_base = '8087b0942f7f3c3961cdfe6cda3b31c26e5a4d0b6bb1557bbb4efe2979e321e5d4daf38756d821c81a5a8c3069d42ecb7fcdb4eb51961e254816f170ce8391b9'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+MagazineMaker::Application.config.session_store :cookie_store, key: '_MagazineMaker_session'
